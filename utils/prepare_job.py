a1537af4a869aaaa74ff1b80c84adfa282ce21d8
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-02 00:08:23 +0100

    Update prepare_job.py

--- utils/prepare_job.py
+++ utils/prepare_job.py
@@ -9,13 +9,14 @@ JOB_FILE_NAME = 'job.py'
 
 # need to think of a better way of doing this... you are showing the key within Kaggle this way. Maybe better to push a dataset of the tests?
 JOB_LINES = [
-  'import os',
+  'import os',  
   #f'!git clone https://{GIT_ACCESS_TOKEN}@github.com/{GIT_USERNAME}/{GIT_REPO_NAME}',
   #'!pip install .',
   #f'!pytest {GIT_REPO_NAME}/{TEST_FOLDER}'
   f'os.system("git clone https://{GIT_ACCESS_TOKEN}@github.com/{GIT_USERNAME}/{GIT_REPO_NAME}")',
+  f'os.chdir("{GIT_REPO_NAME}")',
   'os.system("pip install .")',
-  f'os.system("pytest {GIT_REPO_NAME}/{TEST_FOLDER}")'
+  f'os.system("pytest {TEST_FOLDER}")'
 ]
 
 def prepare_metadata_file():
