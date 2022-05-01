32408864296880601e2d3e8b560113b3a34edc87
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-02 00:53:39 +0100

    Update worker.py

--- utils/worker.py
+++ utils/worker.py
@@ -1,12 +1,24 @@
 import os
+import time
+
+SLEEP_TIME = 5
 GIT_REPO_NAME = os.environ.get('REPO_NAME').split('/')[-1]
 
 def main():
   while True:
     
     output = subprocess.check_output([f'kaggle kernels status {GIT_REPO_NAME}'])
-    break
-  print(output)
+    if 'error' in output:
+      # get logs
+      # output the logs
+      # raise an exception (make sure the logs show)
+      break
+    elif 'success':
+      # double check this
+      # but if success write message and then end the loop
+      break
+    else:
+      time.sleep(SLEEP_TIME)
 
 if __name__ == '__main__':
-  pass
+  main()
