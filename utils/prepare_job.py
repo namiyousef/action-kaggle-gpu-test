7db04fa474ab5f2548a7537e276956869e100c38
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-01 23:20:36 +0100

    Create prepare_job.py

---
+++ utils/prepare_job.py
@@ -0,0 +1,41 @@
+import os
+import json
+
+GIT_USERNAME, GIT_REPO_NAME = os.environ.get('REPO_NAME').split('/')
+GIT_ACCESS_TOKEN = os.environ.get('GIT_ACCESS_TOKEN')
+TEST_FOLDER = os.environ.get('TEST_FOLDER')
+
+JOB_FILE_NAME = 'job.py'
+
+# need to think of a better way of doing this... you are showing the key within Kaggle this way. Maybe better to push a dataset of the tests?
+JOB_LINES = [
+  f'!git clone '
+]
+
+def prepare_metadata_file():
+  
+  with open('kaggle-metadata.json', 'r') as f:
+    metadata_file = json.load(f)
+    
+  metadata_file['id'] = '/'.join([metadata_file['id'].split('/')[0], GIT_REPO_NAME])
+  metadata_file['title'] = GIT_REPO_NAME
+  metadata_file['code_file'] = JOB_FILE_NAME
+  metadata_file['language'] = 'python'
+  metadata_file['kernel_type'] = 'script'
+  metadata_file['enable_gpu'] = True
+  
+  with open('kaggle-metadata.json', 'w') as f:
+    json.dump(metadata_file, f)
+
+def prepare_job_file():
+  with open(JOB_FILE_NAME, 'w') as f:
+    f.writelines()
+
+
+def main():
+  pass
+  
+  
+  
+if __name__ == '__main__':
+  main()
