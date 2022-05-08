48e4e87e25e3594f4e4b544ed0ccdb337aaf91f6
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-08 17:38:06 +0200

    Update worker.py

--- utils/worker.py
+++ utils/worker.py
@@ -1,7 +1,7 @@
 import os
 import time
 import subprocess
-from utils import parser
+from parser import parse_kaggle_output
 
 SLEEP_TIME = 5
 GIT_REPO_NAME = os.environ.get('REPO_NAME').split('/')[-1]
