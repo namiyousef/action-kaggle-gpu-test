155665714270f50e7a5684602cf66f8277a9c50b
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-08 17:48:59 +0200

    Update parser.py

--- utils/parser.py
+++ utils/parser.py
@@ -1,3 +1,9 @@
-print('Import success')
+import json
+
 def parse_kaggle_output(filename):
-  pass
+    output_str = ''
+    with open(f'{filename}.log', 'r') as f:
+        json_output = json.load(f)
+        for line in json_output:
+
+            output_str += line.get('data')
