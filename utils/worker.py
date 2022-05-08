82fd6b424451fbc1557fce5ff5411b169cb28580
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-08 17:24:20 +0200

    Update worker.py

--- utils/worker.py
+++ utils/worker.py
@@ -8,7 +8,7 @@ GIT_REPO_NAME = os.environ.get('REPO_NAME').split('/')[-1]
 def main():
   while True:
     
-    output = subprocess.check_output([f'kaggle kernels status {GIT_REPO_NAME}'])
+    output = subprocess.check_output(['kaggle', 'kernels', 'status', GIT_REPO_NAME])
     if 'error' in output:
       # get logs
       # output the logs
