49787534c18b17498b4162c20dabc5162d5dff29
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-05-08 17:32:09 +0200

    Update worker.py

--- utils/worker.py
+++ utils/worker.py
@@ -1,6 +1,7 @@
 import os
 import time
 import subprocess
+from utils
 
 SLEEP_TIME = 5
 GIT_REPO_NAME = os.environ.get('REPO_NAME').split('/')[-1]
@@ -9,14 +10,17 @@ def main():
   while True:
     
     output = subprocess.check_output(['kaggle', 'kernels', 'status', GIT_REPO_NAME])
+    output = output.decode("utf-8")
+    print(output)
     if 'error' in output:
-      # get logs
-      # output the logs
+      print('Test failed. Full logs below:')
+      # download outputs in log file
+      
+      output = subprocess.check_output(['kaggle', 'kernels', 'output'])
       # raise an exception (make sure the logs show)
       break
     elif 'success':
-      # double check this
-      # but if success write message and then end the loop
+      print('Kaggle Integration Test SUCCESS')
       break
     else:
       time.sleep(SLEEP_TIME)
