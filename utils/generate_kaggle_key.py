9a4c352ada5875ec628e2b565f2ca447229fb897
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-10-21 15:57:26 +0100

    Update generate_kaggle_key.py

--- utils/generate_kaggle_key.py
+++ utils/generate_kaggle_key.py
@@ -15,16 +15,19 @@ ch.setFormatter(formatter)
 LOGGER.addHandler(ch)
 
 def main():
+    
     LOGGER.info('Loading Kaggle API Key')
     kaggle_api_key = os.environ.get('KAGGLE_API_KEY')
     if not kaggle_api_key:
         raise Exception('Could not find Kaggle API Key. Please make sure that the key is provided as a GitHub secret under the name "KAGGLE_API_KEY"')
-
+        
+    LOGGER.info('Reading Kaggle API Key as json')
     try:
         kaggle_json = json.loads(kaggle_api_key)
     except json.decoder.JSONDecodeError as e:
         raise Exception('Failed to read API key as JSON.')
-
+        
+    LOGGER.info('Writing Kaggle API Key into json')
     try:
         with open('.kaggle/kaggle.json', 'w') as f:
             json.dump(kaggle_json, f)
