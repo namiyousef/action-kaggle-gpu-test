3e9fc7d1f29cfba514221974b2dcfe59b9c6def2
Yousef <64047828+namiyousef@users.noreply.github.com>
2022-10-21 15:52:02 +0100

    Update generate_kaggle_key.py

--- utils/generate_kaggle_key.py
+++ utils/generate_kaggle_key.py
@@ -14,22 +14,22 @@ ch.setFormatter(formatter)
 # add ch to logger
 LOGGER.addHandler(ch)
 
+def main():
+    LOGGER.info('Loading Kaggle API Key')
+    kaggle_api_key = os.environ.get('KAGGLE_API_KEY')
+    if not kaggle_api_key:
+        raise Exception('Could not find Kaggle API Key. Please make sure that the key is provided as a GitHub secret under the name "KAGGLE_API_KEY"')
 
-LOGGER.info('Loading Kaggle API Key')
-kaggle_api_key = os.environ.get('KAGGLE_API_KEY')
-if not kaggle_api_key:
-    raise Exception('Could not find Kaggle API Key. Please make sure that the key is provided as a GitHub secret under the name "KAGGLE_API_KEY"')
+    try:
+        kaggle_json = json.loads(kaggle_api_key)
+    except json.decoder.JSONDecodeError as e:
+        raise Exception('Failed to read API key as JSON.')
 
-try:
-    kaggle_json = json.loads(kaggle_api_key)
-except json.decoder.JSONDecodeError as e:
-    raise Exception('Failed to read API key as JSON')
-
-try:
-    with open('.kaggle/kaggle.json', 'w') as f:
-        json.dump(kaggle_json, f)
-except Exception as e:
-    raise Exception('Could not write API key to file')
+    try:
+        with open('.kaggle/kaggle.json', 'w') as f:
+            json.dump(kaggle_json, f)
+    except Exception as e:
+        raise Exception('Could not write API key to file')
     
 if __name__ == '__main__':
   main()
